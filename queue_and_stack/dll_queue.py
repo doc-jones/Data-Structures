d4ce52f66b4b67dea722223dcc72f9f94c8e9037
doc-jones <37349558+doc-jones@users.noreply.github.com>
2019-11-13 15:04:13 -0500

    Add queue

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -7,13 +7,26 @@ class Queue:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # Because you can delete and insert from either the left(head) or the right(tail) and we're done with no need to traverse/loop anything
+        self.storage = DoublyLinkedList()
 
-    def enqueue(self, value):
-        pass
+
+    def enqueue(self, item):
+        self.storage.add_to_tail(item)
+        # increment size counter
+        self.size += 1
 
     def dequeue(self):
-        pass
+        # decrement size counter
+        if self.size > 0:
+            self.size -= 1
+        # remove the head and return it
+        return self.storage.remove_head()
 
     def len(self):
-        pass
+        return self.size
+
+
+
+
+
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -6,7 +6,9 @@ class Stack:
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # Actually with a stack a single linked list is better because we can
+        # pop and push to the head alone.
+        # self.storage = storage_dll
 
     def push(self, value):
         pass
